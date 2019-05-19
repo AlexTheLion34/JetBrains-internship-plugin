83b09273a7678e7f551f1f03ff5f19a3c6753858
AlexTheLion <alx_pet@mail.ru>
2019-05-20 01:35:36 +0300

    Refactorig

---
+++ src/main/java/plugin/Manager.java
@@ -0,0 +1,77 @@
+package plugin;
+
+import com.intellij.notification.*;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+
+import java.io.File;
+import java.util.*;
+
+public class Manager implements ProjectComponent {
+
+    private final Project myProject;
+
+    private final String POM = "pom.xml";
+    private final String GRADLE = "build.gradle";
+    private final String UNKNOWN = "unknown";
+
+    private String configFileName;
+
+    public Manager(Project project) {
+        myProject = project;
+    }
+
+    @Override
+    public void projectOpened() {
+
+        Notification notification = new Notification("ProjectOpenNotification",
+                                                     "Project type",
+                                                      defineDependencyManager(),
+                                                      NotificationType.INFORMATION);
+        notification.notify(myProject);
+
+        if (!configFileName.equals(UNKNOWN))
+            notification.addAction(NotificationAction.createSimple(configFileName, this::openConfigFile));
+    }
+
+    private String defineDependencyManager() {
+
+        String basePath = myProject.getBasePath();
+
+        if (basePath != null) {
+            File projectDirectory = new File(basePath);
+            List<String> configFiles = new ArrayList<>(Arrays.asList(Objects.requireNonNull(projectDirectory.list())));
+
+            if (configFiles.contains(POM)) {
+                configFileName = POM;
+                return "This is Maven project";
+            }
+            if (configFiles.contains(GRADLE)) {
+                configFileName = GRADLE;
+                return "This is Gradle project";
+            }
+        }
+        configFileName = UNKNOWN;
+        return "This is unknown plugin";
+    }
+
+    private void openConfigFile() {
+
+        List<VirtualFile> allConfigFiles = new ArrayList<>(FilenameIndex
+                                                     .getVirtualFilesByName(myProject,
+                                                                            configFileName,
+                                                                            GlobalSearchScope.projectScope(myProject)));
+
+        String projectBaseDirectory = myProject.getBaseDir().getPath();
+
+        Optional<VirtualFile> mainConfigFile = allConfigFiles.stream()
+                                                  .filter(file -> file.getPath()
+                                                                  .contains(projectBaseDirectory + "/" + configFileName))
+                                                  .findFirst();
+        mainConfigFile.ifPresent(virtualFile -> new OpenFileDescriptor(myProject, virtualFile).navigate(true));
+    }
+}
